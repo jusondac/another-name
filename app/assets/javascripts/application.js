fca9f68f389c19eb8a87993b244af9e5291c796e
rejka permana <rejkamuhammad@gmail.com>
2020-04-05 15:08:59 +0700

    make it better

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,75 @@
 //= require popper
 //= require jquery3
 //= require bootstrap
+$(document).ready(function(){
+
+  $(".step").click( function() {
+  $(this).addClass("active").prevAll().addClass("active");
+  $(this).nextAll().removeClass("active");
+  });
+
+  $(".step01").click( function() {
+  $("#line-progress").css("width", "3%");
+  $(".discovery").addClass("active").siblings().removeClass("active");
+  });
+
+  $(".step02").click( function() {
+  $("#line-progress").css("width", "25%");
+  $(".strategy").addClass("active").siblings().removeClass("active");
+  });
+
+  $(".step03").click( function() {
+  $("#line-progress").css("width", "50%");
+  $(".creative").addClass("active").siblings().removeClass("active");
+  });
+
+  $(".step04").click( function() {
+  $("#line-progress").css("width", "75%");
+  $(".production").addClass("active").siblings().removeClass("active");
+  });
+
+  $(".step05").click( function() {
+  $("#line-progress").css("width", "100%");
+  $(".analysis").addClass("active").siblings().removeClass("active");
+  });
+  function checkTime(i) {
+      if (i < 10) {
+          i = "0" + i;
+      }
+      return i;
+  }
+
+  function word(time) {
+    if( time >= 10 && time <= 14 ){
+      document.getElementById('word').innerHTML = "Selamat Siang Semua" ;
+    } else if ( time >= 15 && time <= 17 ) {
+      document.getElementById('word').innerHTML = "Selamat Sore Semua" ;
+    } else if ( time >= 19 && time <= 23 ) {
+      document.getElementById('word').innerHTML = "Selamat Malam Semua" ;
+    } else if ( time >= 0 && time <= 3) {
+      document.getElementById('word').innerHTML = "Masih Malam" ;
+    } else if ( time >= 4 && time <= 9) {
+      document.getElementById('word').innerHTML = "Selamat Pagi Semua" ;
+    }
+  }
+
+  function startTime() {
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      word(h);
+      if ( h < 10){
+        h = '0'+h
+      }
+      var ampm = h >= 12 ? ' pm' : ' am';
+      // add a zero in front of numbers<10
+      m = checkTime(m);
+      s = checkTime(s);
+      document.getElementById('time').innerHTML = h + " : " + m + " : " + s;
+      t = setTimeout(function () {
+          startTime()
+      }, 500);
+  }
+  startTime();
+});
